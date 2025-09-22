122c22274e4ff92b9943ecc2cda3906aceed8ebe
Damir Demirovic <ddemir2@babbage.cs.pdx.edu>
2025-09-21 20:22:51 -0700

    commiting from make

---
+++ archive_practice/darkive.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <pwd.h>
+#include "darkive.h"
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+	printf("Hello World!");
+
+
+
+	return 0;
+}
